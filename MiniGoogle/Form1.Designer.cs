3e116af3f99dab84b4378c8ebc831c63c636c48e
Mohossam9 <Mohammedhosam279@gmail.com>
2020-09-16 22:23:10 +0200

    MiniGoogle

---
+++ MiniGoogle/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace MiniGoogle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.Text;
+            this.textBox1.BackColor = System.Drawing.Color.GhostWhite;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.textBox1.Location = new System.Drawing.Point(349, 222);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(649, 43);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Label1
+            // 
+            this.Label1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.Label1.AutoSize = true;
+            this.Label1.BackColor = System.Drawing.Color.Transparent;
+            this.Label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Label1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Label1.Font = new System.Drawing.Font("Stencil", 48F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label1.ForeColor = System.Drawing.SystemColors.Info;
+            this.Label1.Location = new System.Drawing.Point(412, 72);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(535, 97);
+            this.Label1.TabIndex = 1;
+            this.Label1.Text = "Mini Google";
+            this.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkSalmon;
+            this.button1.Font = new System.Drawing.Font("Monotype Corsiva", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Cornsilk;
+            this.button1.Location = new System.Drawing.Point(1022, 216);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 49);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.DarkSalmon;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ControlLight;
+            this.dataGridView1.Location = new System.Drawing.Point(349, 270);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(649, 300);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1372, 582);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.textBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ForeColor = System.Drawing.Color.Crimson;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "MiniGoogle";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ MiniGoogle/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Collections;
+using System.IO;
+
+namespace MiniGoogle
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        public static void Loadfiles(string sourceDir, List<string> allFiles)
+        {
+
+            string[] textnames = Directory.GetFiles(sourceDir);
+            for (int text = 0; text < textnames.Length; text++)
+            {
+                allFiles.Add(textnames[text]);
+            }
+
+        }
+
+
+        public static bool stopwords(string words)
+        {
+
+            if (words.Length < 3)
+            {
+                return true;
+            }
+            else if (words == "this" || words == "That" || words == "those" || words == "they" || words == "The" || words == "our" || words == "these" || words == "off" || words == "has" || words == "have" || words == "are" || words == "them" || words == "could" || words == "for" || words == "can")
+            {
+                return true;
+            }
+
+            return false;
+
+        }
+        public static void Indexfiles(Dictionary<string, List<Tuple<string, int>>> word, List<string> allFiles)
+        {
+
+            foreach (string filename in allFiles)
+            {
+                string[] Lines = File.ReadAllLines(filename);
+                int linenum = 1;
+                foreach (string s in Lines)
+                {
+                    if (s != "")
+                    {
+                        string[] words = s.Split(new Char[] { ' ', ',', '&', '.', '!', '?', '<', '>', '{', '}', '(', ')', '*', '%', ':', ';', '-', '\\', '/', '[', ']', '$', '^' });
+                        addtodic(word, filename, words, linenum);
+                    }
+                    linenum++;
+                }
+            }
+
+        }
+
+        public static void addtodic(Dictionary<string, List<Tuple<string, int>>> word, string filename, string[] words, int Linenumber)
+        {
+
+            for (int w = 0; w < words.Length; w++)
+            {
+                if (!stopwords(words[w]))
+                {
+                    if (word.ContainsKey(words[w]))
+                    {
+                        word[words[w]].Add(Tuple.Create(filename, Linenumber));
+                    }
+                    else
+                    {
+                        List<Tuple<string, int>> newword = new List<Tuple<string, int>>();
+                        newword.Add(Tuple.Create(filename, Linenumber));
+                        word.Add(words[w], newword);
+                    }
+                }
+            }
+
+        }
+
+
+
+        public static void results(Dictionary<string,List<Tuple<string,int>>> Word,string searchWord,DataGridView v)
+        {
+            DataTable res = new DataTable();
+            DataSet ds = new DataSet();
+            ds.Tables.Add(res);
+            DataColumn sw = new DataColumn("Word");
+            DataColumn path = new DataColumn("Filepath");
+            DataColumn line = new DataColumn("Linenumber");
+            res.Columns.Add(sw);
+            res.Columns.Add(path);
+            res.Columns.Add(line);
+            if (searchWord == "")
+            {
+                MessageBox.Show("Please Enter a word !");
+            }
+            else
+            {
+                if (Word.ContainsKey(searchWord))
+                {
+                    
+                    foreach (Tuple<string, int> inlist in Word[searchWord])
+                    {
+                        DataRow rw = res.NewRow();
+                        string pathword = inlist.Item1;
+                        int lineword = inlist.Item2;
+                        rw[0] = searchWord;
+                        rw[1] = pathword;
+                        rw[2] = lineword;
+                        res.Rows.Add(rw);
+                    }
+                    v.DataSource = res;
+
+                }
+                else
+                {
+                    DataRow rw = res.NewRow();
+                    rw[0] = searchWord;
+                    rw[1] = "Notfound";
+                    rw[2] = "Notfound";
+                    res.Rows.Add(rw);
+                    v.DataSource = res;
+                }
+            }
+        }
+
+
+
+
+
+ 
+                      
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, List<Tuple<string, int>>> Word = new Dictionary<string, List<Tuple<string, int>>>();
+            string sourceFolder = "textAll";
+            string searchWord = textBox1.Text;
+            List<string> allFiles = new List<string>();
+            List<Tuple<string, int>> wordinfo = new List<Tuple<string, int>>();
+            Loadfiles(sourceFolder, allFiles);
+            Indexfiles(Word, allFiles);
+            results(Word, searchWord, dataGridView1);
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
